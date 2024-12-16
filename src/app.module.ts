555c5cb8976a94f25064fca829108686f1f8a0a5
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2024-12-16 15:55:55 +0500

    add auth, top-page, product models

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BlogModule } from './blog/blog.module';
+import { AuthModule } from './auth/auth.module';
+import { TopPageModule } from './top-page/top-page.module';
+import { ProductModule } from './product/product.module';
+import { ReviewModule } from './review/review.module';
 
 @Module({
-  imports: [BlogModule],
+  imports: [BlogModule, AuthModule, TopPageModule, ProductModule, ReviewModule],
   controllers: [AppController],
   providers: [AppService],
 })
